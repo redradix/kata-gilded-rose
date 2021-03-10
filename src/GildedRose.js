2a10e5c51f66c721da568df194f6b00064342024
Victor <victor@redradix.com>
2021-03-10 15:47:35 +0100

    make explicit condition and remove duplication

--- src/GildedRose.js
+++ src/GildedRose.js
@@ -55,16 +55,18 @@ GildedRose.updateQuality = function (items) {
         } else {
           item.quality = item.quality - item.quality
         }
-      } else {
+      }
+
+      if (AGED_BRIE === item.name) {
         if (item.quality < 50) {
           item.quality = item.quality + 1
         }
-        if (AGED_BRIE === item.name && item.sellIn <= 0) {
+        if (item.sellIn <= 0) {
           item.quality = 0
         }
       }
     }
-    
+
     if (SULFURAS !== item.name && item.quality > 50) {
       item.quality = 50
     }
