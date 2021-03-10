5087f0157c8b2f08e6cdf50aaa508957628c359c
Victor <victor@redradix.com>
2021-03-10 16:51:07 +0100

    remove duplication

--- src/GildedRose.js
+++ src/GildedRose.js
@@ -38,30 +38,7 @@ GildedRose.updateQuality = function (items) {
       }
     }
 
-    if (BACKSTAGE === item.name) {
-      if (item.quality < 50) {
-        item.quality = item.quality + 1
-
-        if (item.sellIn < 6) {
-          item.quality = item.quality + 1
-        }
-        if (item.sellIn < 11) {
-          item.quality = item.quality + 1
-        }
-      }
-
-      if (item.quality > 50) {
-        item.quality = 50
-      }
-
-      item.sellIn = item.sellIn - 1
-      
-      if (item.sellIn < 0) {
-        item.quality = 0
-      }
-    }
-
-    if (AGED_BRIE === item.name) {
+    if (BACKSTAGE === item.name || AGED_BRIE === item.name) {
       if (item.quality < 50) {
         item.quality = item.quality + 1
 
@@ -80,9 +57,6 @@ GildedRose.updateQuality = function (items) {
       item.sellIn = item.sellIn - 1
 
       if (item.sellIn < 0) {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1
-        }
         item.quality = 0
       }
     }
