c41b496b6b2e31fcfa77e97eb1f341b3dd16e20f
Victor <victor@redradix.com>
2021-03-10 15:42:32 +0100

    compact ifs

--- src/GildedRose.js
+++ src/GildedRose.js
@@ -21,35 +21,23 @@ const GildedRose = function () {
 GildedRose.updateQuality = function (items) {
   for (var i = 0; i < items.length; i++) {
     const item = items[i]
-    if (AGED_BRIE !== item.name && BACKSTAGE !== item.name) {
-      if (item.quality > 0) {
-        if (SULFURAS !== item.name) {
-          item.quality = item.quality - 1
-        }
-      }
+    if (AGED_BRIE !== item.name && BACKSTAGE !== item.name && item.quality > 0 && SULFURAS !== item.name) {
+      item.quality = item.quality - 1
     } else {
       if (item.quality < 50) {
         item.quality = item.quality + 1
-        if (AGED_BRIE === item.name) {
-            if (item.sellIn < 6) {
-              item.quality = item.quality + 1
-            }
+        if (AGED_BRIE === item.name && item.sellIn < 6) {
+          item.quality = item.quality + 1
         }
-        if (AGED_BRIE === item.name) {
-            if (item.sellIn < 11) {
-              item.quality = item.quality + 1
-            }
+        if (AGED_BRIE === item.name && item.sellIn < 11) {
+          item.quality = item.quality + 1
         }
         if (BACKSTAGE === item.name) {
-          if (item.sellIn < 11) {
-            if (item.quality < 50) {
-              item.quality = item.quality + 1
-            }
+          if (item.sellIn < 11 && item.quality < 50) {
+            item.quality = item.quality + 1
           }
-          if (item.sellIn < 6) {
-            if (item.quality < 50) {
-              item.quality = item.quality + 1
-            }
+          if (item.sellIn < 6 && item.quality < 50) {
+            item.quality = item.quality + 1
           }
         }
       }
@@ -59,12 +47,8 @@ GildedRose.updateQuality = function (items) {
     }
     if (item.sellIn < 0) {
       if (AGED_BRIE !== item.name) {
-        if (BACKSTAGE !== item.name) {
-          if (item.quality > 0) {
-            if (SULFURAS !== item.name) {
-              item.quality = item.quality - 1
-            }
-          }
+        if (BACKSTAGE !== item.name && item.quality > 0 && SULFURAS !== item.name) {
+          item.quality = item.quality - 1
         } else {
           item.quality = item.quality - item.quality
         }
@@ -72,12 +56,14 @@ GildedRose.updateQuality = function (items) {
         if (item.quality < 50) {
           item.quality = item.quality + 1
         }
-        if (AGED_BRIE === item.name && item.sellIn <= 0)
-            item.quality = 0
+        if (AGED_BRIE === item.name && item.sellIn <= 0) {
+          item.quality = 0
+        }
       }
     }
-    if (SULFURAS !== item.name)
-      if (item.quality > 50) item.quality = 50
+    if (SULFURAS !== item.name && item.quality > 50) {
+      item.quality = 50
+    }
   }
   return items
 };
