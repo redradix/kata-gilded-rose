504ac4a92947b895275583d507b9b00f732d1453
Victor <victor@redradix.com>
2021-03-10 15:59:21 +0100

    split 'concept' ifs

--- src/GildedRose.js
+++ src/GildedRose.js
@@ -22,17 +22,23 @@ GildedRose.updateQuality = function (items) {
   for (var i = 0; i < items.length; i++) {
     const item = items[i]
 
-    if (AGED_BRIE !== item.name && BACKSTAGE !== item.name && SULFURAS !== item.name && item.quality > 0) {
-      item.quality = item.quality - 1
+    if (AGED_BRIE !== item.name && BACKSTAGE !== item.name && SULFURAS !== item.name) {
+      if (item.quality > 0) {
+        item.quality = item.quality - 1
+      }
     } else {
       if (item.quality < 50) {
         item.quality = item.quality + 1
-        if (AGED_BRIE === item.name && item.sellIn < 6) {
-          item.quality = item.quality + 1
+
+        if (AGED_BRIE === item.name) {
+          if (item.sellIn < 6) {
+            item.quality = item.quality + 1
+          }
+          if (item.sellIn < 11) {
+            item.quality = item.quality + 1
+          }
         }
-        if (AGED_BRIE === item.name && item.sellIn < 11) {
-          item.quality = item.quality + 1
-        }
+
         if (BACKSTAGE === item.name) {
           if (item.sellIn < 11 && item.quality < 50) {
             item.quality = item.quality + 1
@@ -46,12 +52,17 @@ GildedRose.updateQuality = function (items) {
 
     if (SULFURAS !== item.name) {
       item.sellIn = item.sellIn - 1
+      if (item.quality > 50) {
+        item.quality = 50
+      }
     }
 
     if (item.sellIn < 0) {
       if (AGED_BRIE !== item.name) {
-        if (BACKSTAGE !== item.name && SULFURAS !== item.name && item.quality > 0) {
-          item.quality = item.quality - 1
+        if (BACKSTAGE !== item.name && SULFURAS !== item.name) {
+          if (item.quality > 0) {
+            item.quality = item.quality - 1
+          }
         } else {
           item.quality = item.quality - item.quality
         }
@@ -65,10 +76,6 @@ GildedRose.updateQuality = function (items) {
           item.quality = 0
         }
       }
-    }
-
-    if (SULFURAS !== item.name && item.quality > 50) {
-      item.quality = 50
     }
   }
   return items
