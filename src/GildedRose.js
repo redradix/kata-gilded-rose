119b3dce6a1a1163b34a5bfb21c4a9169596faeb
Victor <victor@redradix.com>
2021-03-10 16:18:48 +0100

    compact if

--- src/GildedRose.js
+++ src/GildedRose.js
@@ -55,10 +55,8 @@ GildedRose.updateQuality = function (items) {
     }
 
     if (BACKSTAGE !== item.name && SULFURAS !== item.name) {
-      if (item.sellIn < 0) {
-        if (item.quality > 0) {
-          item.quality = item.quality - 1
-        }
+      if (item.sellIn < 0 && item.quality > 0) {
+        item.quality = item.quality - 1
       }
     } else {
       if (item.sellIn < 0) {
