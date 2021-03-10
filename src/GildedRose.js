5c284c99c8136756a221af82cf601123e6fd6682
Victor <victor@redradix.com>
2021-03-10 16:43:46 +0100

    merge ifs

--- src/GildedRose.js
+++ src/GildedRose.js
@@ -32,6 +32,10 @@ GildedRose.updateQuality = function (items) {
       }
 
       item.sellIn = item.sellIn - 1
+
+      if (item.sellIn < 0 && item.quality > 0) {
+        item.quality = item.quality - 1
+      }
     } 
 
     if (AGED_BRIE === item.name || BACKSTAGE === item.name) {
@@ -51,12 +55,6 @@ GildedRose.updateQuality = function (items) {
       }
 
       item.sellIn = item.sellIn - 1
-    }
-
-    if (ELIXIR === item.name || VEST === item.name || CONJURED === item.name) {
-      if (item.sellIn < 0 && item.quality > 0) {
-        item.quality = item.quality - 1
-      }
     }
 
     if (BACKSTAGE === item.name) {
