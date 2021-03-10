5207da0ea267e3514b62713cdd879314bb61d3f3
Victor <victor@redradix.com>
2021-03-10 16:06:51 +0100

    remove useless if

--- src/GildedRose.js
+++ src/GildedRose.js
@@ -49,14 +49,12 @@ GildedRose.updateQuality = function (items) {
     }
 
     if (item.sellIn < 0) {
-      if (AGED_BRIE !== item.name) {
-        if (BACKSTAGE !== item.name && SULFURAS !== item.name) {
-          if (item.quality > 0) {
-            item.quality = item.quality - 1
-          }
-        } else {
-          item.quality = item.quality - item.quality
+      if (BACKSTAGE !== item.name && SULFURAS !== item.name) {
+        if (item.quality > 0) {
+          item.quality = item.quality - 1
         }
+      } else {
+        item.quality = item.quality - item.quality
       }
 
       if (AGED_BRIE === item.name) {
