9a6fc90877fc20a6b6f615baabb34dc4110ee0e2
Victor <victor@redradix.com>
2021-03-10 16:44:55 +0100

    divide if block into explicit products

--- src/GildedRose.js
+++ src/GildedRose.js
@@ -36,9 +36,9 @@ GildedRose.updateQuality = function (items) {
       if (item.sellIn < 0 && item.quality > 0) {
         item.quality = item.quality - 1
       }
-    } 
+    }
 
-    if (AGED_BRIE === item.name || BACKSTAGE === item.name) {
+    if (BACKSTAGE === item.name) {
       if (item.quality < 50) {
         item.quality = item.quality + 1
 
@@ -55,15 +55,30 @@ GildedRose.updateQuality = function (items) {
       }
 
       item.sellIn = item.sellIn - 1
-    }
-
-    if (BACKSTAGE === item.name) {
+      
       if (item.sellIn < 0) {
         item.quality = 0
       }
     }
 
     if (AGED_BRIE === item.name) {
+      if (item.quality < 50) {
+        item.quality = item.quality + 1
+
+        if (item.sellIn < 6) {
+          item.quality = item.quality + 1
+        }
+        if (item.sellIn < 11) {
+          item.quality = item.quality + 1
+        }
+      }
+
+      if (item.quality > 50) {
+        item.quality = 50
+      }
+
+      item.sellIn = item.sellIn - 1
+
       if (item.sellIn < 0) {
         if (item.quality < 50) {
           item.quality = item.quality + 1
