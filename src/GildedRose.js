c347acacc2c587c3edfa91120b64bb40da2bb0ba
Virgilio Gómez <virgilio@redradix.com>
2021-10-20 09:52:32 +0200

    First (and best?) approach

--- src/GildedRose.js
+++ src/GildedRose.js
@@ -1,82 +1,64 @@
-import Item from './Item'
-
-const GildedRose = function () {
-  var items = []
-  items.push(new Item("+5 Dexterity Vest", 10, 20))
-  items.push(new Item("Aged Brie", 2, 0))
-  items.push(new Item("Elixir of the Mongoose", 5, 7))
-  items.push(new Item("Sulfuras, Hand of Ragnaros", 0, 80))
-  items.push(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20))
-  items.push(new Item("Conjured Mana Cake", 3, 6))
-  GildedRose.updateQuality(items)
-}
-
-GildedRose.updateQuality = function (items) {
-  for (var i = 0; i < items.length; i++) {
-    if ("Aged Brie" != items[i].name && "Backstage passes to a TAFKAL80ETC concert" != items[i].name) {
-      //TODO: Improve this code.
-      if (items[i].quality > 0) {
-        if ("Sulfuras, Hand of Ragnaros" != items[i].name) {
-          items[i].quality = items[i].quality - 1
-        }
-      }
-    } else {
-      if (items[i].quality < 50) {
-        items[i].quality = items[i].quality + 1
-        if ("Aged Brie" == items[i].name) {
-            if (items[i].sellIn < 6) {
-              items[i].quality = items[i].quality + 1
-            }
-        }
-        //Increases the Quality of the stinky cheese if its 11 days to due date.
-        if ("Aged Brie" == items[i].name) {
-            if (items[i].sellIn < 11) {
-              items[i].quality = items[i].quality + 1
-            }
-        }
-        if ("Backstage passes to a TAFKAL80ETC concert" == items[i].name) {
-          if (items[i].sellIn < 11) {
-            // See revision number 2394 on SVN.
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
-            }
-          }
-          //Increases the Quality of Backstage Passes if the Quality is 6 or less.
-          if (items[i].sellIn < 6) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
-            }
-          }
-        }
-      }
-    }
-    if ("Sulfuras, Hand of Ragnaros" != items[i].name) {
-      items[i].sellIn = items[i].sellIn - 1
-    }
-    if (items[i].sellIn < 0) {
-      if ("Aged Brie" != items[i].name) {
-        if ("Backstage passes to a TAFKAL80ETC concert" != items[i].name) {
-          if (items[i].quality > 0) {
-            if ("Sulfuras, Hand of Ragnaros" != items[i].name) {
-              items[i].quality = items[i].quality - 1
-            }
-          }
-        } else {
-          //TODO: Fix this.
-          items[i].quality = items[i].quality - items[i].quality
-        }
-      } else {
-        if (items[i].quality < 50) {
-          items[i].quality = items[i].quality + 1
-        }
-        if ("Aged Brie" == items[i].name && items[i].sellIn <= 0)
-            items[i].quality = 0
-      } // of for.
-    }
-    if ("Sulfuras, Hand of Ragnaros" != items[i].name)
-      if (items[i].quality > 50) items[i].quality = 50
-  }
-  return items
+import Item from './Item';
+ 
+const GildedRose = function GildedRose() {
+ const items = [];
+ items.push(new Item('+5 Dexterity Vest', 10, 20));
+ items.push(new Item('Aged Brie', 2, 0));
+ items.push(new Item('Elixir of the Mongoose', 5, 7));
+ items.push(new Item('Sulfuras, Hand of Ragnaros', 0, 80));
+ items.push(new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20));
+ items.push(new Item('Conjured Mana Cake', 3, 6));
+ GildedRose.updateQuality(items);
 };
-
-export default GildedRose
+ 
+function updateItemQuality(item) {
+ // Prevent modifying Sulfuras
+ if (item.name !== 'Sulfuras, Hand of Ragnaros') {
+   // Decrease sellIn
+   item.sellIn -= 1;
+   // Set regular quality modifier to -1
+   let qualityModifier = -1;
+   if (item.name === 'Aged Brie' || item.name === 'Backstage passes to a TAFKAL80ETC concert') {
+     /*
+       Aged Brie and Backstage quality behaviour:
+         - increases by 1 as sellIn decreases
+         - increases by 2 when there are 10 days or less
+         - increases by 3 when there are 5 days or less
+         - drops to 0 when sellIn is less than 0
+     */
+     qualityModifier = 1;
+     if (item.sellIn < 11) {
+       qualityModifier += 1;
+     }
+     if (item.sellIn < 6) {
+       qualityModifier += 1;
+     }
+     if (item.sellIn < 0) {
+       qualityModifier = item.quality * -1;
+     }
+   } else {
+     // The quality of the rest of items degrades twice as fast once the sell date has passed
+     if (item.sellIn < 0) {
+       qualityModifier *= 2;
+     }
+     // Conjured quality degrades twice as fast as normal items
+     if (item.name === 'Conjured Mana Cake') {
+       qualityModifier *= 2;
+     }
+   }
+   // Apply quality modifier
+   item.quality += qualityModifier;
+   // Prevent quality being less than 0 of greater than 50
+   if (item.quality < 0) {
+     item.quality = 0;
+   } else if (item.quality > 50) {
+     item.quality = 50;
+   }
+ }
+}
+ 
+GildedRose.updateQuality = function updateQuality(items) {
+ items.forEach(updateItemQuality);
+};
+ 
+export default GildedRose;
