d1337e15f144515ca51bab57541dfdd820feaa0f
Ester Legaz <ester@redradix.com>
2022-05-26 12:04:58 +0200

    refactor: simplify logic for all items

--- src/GildedRose.js
+++ src/GildedRose.js
@@ -11,50 +11,18 @@ const GildedRose = () => {
   GildedRose.updateQuality(items);
 };
 
-const isDesgradedItem = (itemName) =>
-  itemName.toLowerCase().includes("conjured");
-
 const isIncreasedQualityItem = (itemName) =>
   itemName.toLowerCase().includes("aged brie") ||
   itemName.toLowerCase().includes("backstage passes");
 
-const isLegendaryItem = (itemName) =>
-  itemName.toLowerCase().includes("sulfuras");
-
 GildedRose.updateQuality = function (items) {
   for (let i = 0; i < items.length; i++) {
-    if (isIncreasedQualityItem(items[i].name)) {
-      treatIncreasedQualityItems(items, i);
-    } else {
-      //TODO: Improve this code.
-      if (items[i].quality > 0) {
-        if ("Sulfuras, Hand of Ragnaros" != items[i].name) {
-          items[i].quality = items[i].quality - 1;
-        }
-      }
-    }
-    if ("Sulfuras, Hand of Ragnaros" != items[i].name) {
+    if (items[i].quality < 50) {
       items[i].sellIn = items[i].sellIn - 1;
-      if (items[i].quality > 50) items[i].quality = 50;
-    }
-    if (items[i].sellIn < 0) {
-      if ("Aged Brie" != items[i].name) {
-        if ("Backstage passes to a TAFKAL80ETC concert" != items[i].name) {
-          if (items[i].quality > 0) {
-            if ("Sulfuras, Hand of Ragnaros" != items[i].name) {
-              items[i].quality = items[i].quality - 1;
-            }
-          }
-        } else {
-          //TODO: Fix this.
-          items[i].quality = items[i].quality - items[i].quality;
-        }
+      if (isIncreasedQualityItem(items[i].name)) {
+        treatIncreasedQualityItems(items, i);
       } else {
-        if (items[i].quality < 50) {
-          items[i].quality = items[i].quality + 1;
-        }
-        if ("Aged Brie" == items[i].name && items[i].sellIn <= 0)
-          items[i].quality = 0;
+        treatNormalItems(items, i);
       }
     }
   }
@@ -62,17 +30,33 @@ GildedRose.updateQuality = function (items) {
 };
 
 const treatIncreasedQualityItems = (items, i) => {
-  if (items[i].quality < 50) {
+  items[i].quality = items[i].quality + 1;
+
+  if (items[i].sellIn < 11) {
     items[i].quality = items[i].quality + 1;
+  }
 
-    if (items[i].sellIn < 11) {
-      items[i].quality = items[i].quality + 1;
-    }
+  if (items[i].sellIn < 6) {
+    items[i].quality = items[i].quality + 1;
+  }
 
-    if (items[i].sellIn < 6) {
-      items[i].quality = items[i].quality + 1;
+  if (items[i].sellIn <= 0) {
+    items[i].quality = 0;
+  }
+
+  if (items[i].quality > 50) items[i].quality = 50;
+
+  return items;
+};
+
+const treatNormalItems = (items, i) => {
+  if (items[i].quality > 0) {
+    items[i].quality = items[i].quality - 1;
+    if (items[i].sellIn < 0) {
+      items[i].quality = items[i].quality - 1;
     }
   }
+
   return items;
 };
 
